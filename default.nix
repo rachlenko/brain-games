12953ccd6f8e7bd07ace8860ecb9746eeef7a2e4
Evgeny Rachlenko <evgeny.rachlenko@gmail.com>
2021-10-04 14:43:35 +0000

    changed poetry topology

--- default.nix
+++ default.nix
@@ -14,11 +14,14 @@ in
     ];
    shellHook = ''
      echo "running python here "
-     python -m virtualenv venv
-     source venv/bin/activate
-     pip install -r requirements.txt
+     #python -m virtualenv venv
+     #source venv/bin/activate
+     #pip install -r requirements.txt
+
      curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
      source $HOME/.poetry/env
+     [ -d brain-games ] || echo "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+     poetry install
      pre-commit install
       '';
   }
